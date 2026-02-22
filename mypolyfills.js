fb817a2526d4b10c893c77463aaa842d01ddab08
omkarg419 <68603231+omkarg419@users.noreply.github.com>
2026-02-22 18:32:44 +0530

    feat: add forMap and myReduce polyfills

--- mypolyfills.js
+++ mypolyfills.js
@@ -4,3 +4,42 @@ Array.prototype.myForEach=function(funcAsArgu){
         funcAsArgu(this[i],i,this)
     }
 }
+
+Array.prototype.forMap=function(callbackfn)
+{
+    const result=[]
+
+    for(let i=0;i<this.length;i++)
+    {
+        result.push(callbackfn(this[i],i,this,))
+    }
+
+    return result
+}
+
+Array.prototype.myReduce=function(callbackfn,initialValue)
+{
+    let accumulator;
+    let startIndex=0;
+
+    if(initialValue !== undefined)
+    {
+        accumulator=initialValue
+    }
+    else{
+        accumulator=this[0]
+        startIndex=1
+    }
+
+    for(let i= startIndex; i<this.length; i++)
+    {
+        accumulator=callbackfn(accumulator,this[i],i,this)
+    }
+return accumulator
+    
+}
+
+
+
+
+
